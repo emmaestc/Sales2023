4496c0376f9356b51e913060941b32831c343cba
emmaestc <emmanuelestrada898@gmail.com>
2023-02-28 18:48:25 -0500

    Hasta clase 5

--- Sales/Sales.shared/Entities/State.cs
+++ Sales/Sales.shared/Entities/State.cs
@@ -4,20 +4,23 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Sales.shared.Entities
 {
-    public class Country
+    public class State
     {
         public int Id { get; set; }
 
-        [Display (Name = "Pais")]
+        [Display(Name = "Estado/Departamento")]
         [Required(ErrorMessage = "El campo {0} es obligatorio")]
         [MaxLength(100, ErrorMessage = "El campo {0} no puede contener mas de {1} caracteres")]
         public string Name { get; set; } = null!;
 
-        public ICollection<State>? States { get; set; }
+        public Country? Country { get; set; } // con esto entity framework sabe que hay una relacion 1 a m
 
-        public int StatesNumber => States == null ? 0 : States.Count;
+        public ICollection<City>? Cities { get; set; }
+
+        public int CitiesNumber => Cities == null ? 0 : Cities.Count;
     }
 }
