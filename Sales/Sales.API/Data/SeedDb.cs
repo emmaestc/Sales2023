ad528363bf6d43fba3944d39ed92435dd2369408
emmaestc <emmanuelestrada898@gmail.com>
2023-03-03 15:58:42 -0500

    Hasta Actividad 1

--- Sales/Sales.API/Data/SeedDb.cs
+++ Sales/Sales.API/Data/SeedDb.cs
@@ -18,7 +18,7 @@ namespace Sales.API.Data
             await CheckCountriesAsync();
         }
 
-        public async Task SeederAsync()
+        public async Task SeedCTAsync()
         {
             await _context.Database.EnsureCreatedAsync();
             await CheckCategoriesAsync();
@@ -41,9 +41,20 @@ namespace Sales.API.Data
         {
             if (!_context.Categories.Any()) 
             {
-                _context.Categories.Add(new Category { Name = "Hogar" });
-                _context.Categories.Add(new Category { Name = "Moda y belleza" });
-                _context.Categories.Add(new Category { Name = "Tecnología" });
+                _context.Categories.Add(new Category { Name = "Accesorios para vehiculos" });
+                _context.Categories.Add(new Category { Name = "Agro" });
+                _context.Categories.Add(new Category { Name = "Alimentos y Bebidas" });
+                _context.Categories.Add(new Category { Name = "Animales y Mascotas" });
+                _context.Categories.Add(new Category { Name = "Bebés" });
+                _context.Categories.Add(new Category { Name = "Belleza y Cuidado ¨Personal" });
+                _context.Categories.Add(new Category { Name = "Celulares y Telefonos" });
+                _context.Categories.Add(new Category { Name = "Consolas y Videojuegos" });
+                _context.Categories.Add(new Category { Name = "Construcción" });
+                _context.Categories.Add(new Category { Name = "Deportes y Fitness" });
+                _context.Categories.Add(new Category { Name = "Electrodomésticos" });
+                _context.Categories.Add(new Category { Name = "Hogar y muebles" });
+                _context.Categories.Add(new Category { Name = "Instrumentos musicales" });
+                _context.Categories.Add(new Category { Name = "Inmuebles" });
                 await _context.SaveChangesAsync();
             }
 
