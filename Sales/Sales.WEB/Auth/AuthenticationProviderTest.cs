951ddcf079fde395fc57f61603bbbb26463e1c97
emmaestc <emmanuelestrada898@gmail.com>
2023-03-14 18:36:50 -0500

    Hasta clase 9

---
+++ Sales/Sales.WEB/Auth/AuthenticationProviderTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Sales.WEB.Auth
+{
+    public class AuthenticationProviderTest : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var anonimous = new ClaimsIdentity();
+            var emmaUser = new ClaimsIdentity(new List<Claim>
+        {
+            new Claim("FirstName", "Emma"),
+            new Claim("LastName", "Estrada"),
+            new Claim(ClaimTypes.Name, "emma@yopmail.com"),
+            new Claim(ClaimTypes.Role, "Admin")
+        },
+        authenticationType: "test");
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(emmaUser)));
+        }
+
+    }
+}
